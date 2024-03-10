b89cd3d4246823849c69b973713645cb71edcf17
Lakello <winmiloni@mail.ru>
2024-03-10 22:41:29 +0700

    Text

---
+++ Assets/Source/Scripts/Game/UI/WordWobble.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace CubeProject.UI
+{
+	public class WordWobble : MonoBehaviour
+	{
+		[SerializeField] private Gradient rainbow;
+		[SerializeField] private float _heightOffsetSpeed = 3.3f;
+		[SerializeField] private float _widthOffsetSpeed = 2.5f;
+
+		private TMP_Text textMesh;
+		private Mesh mesh;
+		private Vector3[] vertices;
+		private List<int> wordIndexes;
+		private List<int> wordLengths;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			textMesh = GetComponent<TMP_Text>();
+
+			wordIndexes = new List<int>
+			{
+				0
+			};
+
+			wordLengths = new List<int>();
+
+			string s = textMesh.text;
+
+			for (int index = s.IndexOf(' '); index > -1; index = s.IndexOf(' ', index + 1))
+			{
+				wordLengths.Add(index - wordIndexes[wordIndexes.Count - 1]);
+				wordIndexes.Add(index + 1);
+			}
+
+			wordLengths.Add(s.Length - wordIndexes[wordIndexes.Count - 1]);
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			textMesh.ForceMeshUpdate();
+			mesh = textMesh.mesh;
+			vertices = mesh.vertices;
+
+			Color[] colors = mesh.colors;
+
+			Debug.Log($"Vertices = {vertices.Length}");
+			Debug.Log($"colors = {colors.Length}");
+			
+			for (int w = 0; w < wordIndexes.Count; w++)
+			{
+				int wordIndex = wordIndexes[w];
+				Vector3 offset = Wobble(Time.time + w);
+
+				for (int i = 0; i < wordLengths[w]; i++)
+				{
+					TMP_CharacterInfo c = textMesh.textInfo.characterInfo[wordIndex + i];
+
+					int index = c.vertexIndex;
+
+					colors[index] = rainbow.Evaluate(Mathf.Repeat(Time.time + vertices[index].x * 0.001f, 1f));
+					colors[index + 1] = rainbow.Evaluate(Mathf.Repeat(Time.time + vertices[index + 1].x * 0.001f, 1f));
+					colors[index + 2] = rainbow.Evaluate(Mathf.Repeat(Time.time + vertices[index + 2].x * 0.001f, 1f));
+					colors[index + 3] = rainbow.Evaluate(Mathf.Repeat(Time.time + vertices[index + 3].x * 0.001f, 1f));
+
+					vertices[index] += offset;
+					vertices[index + 1] += offset;
+					vertices[index + 2] += offset;
+					vertices[index + 3] += offset;
+				}
+			}
+
+			mesh.vertices = vertices;
+			mesh.colors = colors;
+			textMesh.canvasRenderer.SetMesh(mesh);
+		}
+
+		Vector2 Wobble(float time)
+		{
+			return new Vector2(Mathf.Sin(time * _heightOffsetSpeed), Mathf.Cos(time * _widthOffsetSpeed));
+		}
+	}
+}
