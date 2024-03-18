6cb4a4b52ac8e00dcf039d86e71fc755aed25c05
Lakello <slavka779@mail.ru>
2024-03-18 20:02:00 +0700

    Fix cube view

--- Assets/Source/Scripts/Game/Player/CubeView.cs
+++ Assets/Source/Scripts/Game/Player/CubeView.cs
@@ -7,26 +7,39 @@ namespace CubeProject.PlayableCube
 {
 	public class CubeView : MonoBehaviour
 	{
-		private const string LightColor = "_LightColor";
+		private const string HeartColorProperty = "_HeartColor";
+		private const string EnergyColorProperty = "_EnergyProperty";
 
-		[SerializeField] private HDRColorChanger _hdrColorChanger;
+		[SerializeField] private HDRColorChanger _energyColorChanger;
+		[SerializeField] private HDRColorChanger _heartColorChanger;
 
 		private ChargeHolder _chargeHolder;
 
 		private void Awake()
 		{
 			gameObject.GetComponentElseThrow(out _chargeHolder);
-			_hdrColorChanger.Init(() => _chargeHolder.IsCharged);
-			_hdrColorChanger.Init(LightColor);
+			
+			InitColorChanger(_energyColorChanger, EnergyColorProperty);
+			InitColorChanger(_heartColorChanger, HeartColorProperty);
 		}
 
 		private void OnEnable()
 		{
-			_chargeHolder.ChargeChanged += _hdrColorChanger.ChangeColor;
-			_hdrColorChanger.ChangeColor();
+			_chargeHolder.ChargeChanged += _energyColorChanger.ChangeColor;
+			_chargeHolder.ChargeChanged += _heartColorChanger.ChangeColor;
+			_energyColorChanger.ChangeColor();
 		}
 
-		private void OnDisable() =>
-			_chargeHolder.ChargeChanged -= _hdrColorChanger.ChangeColor;
+		private void OnDisable()
+		{
+			_chargeHolder.ChargeChanged -= _energyColorChanger.ChangeColor;
+			_chargeHolder.ChargeChanged -= _heartColorChanger.ChangeColor;
+		}
+
+		private void InitColorChanger(HDRColorChanger changer, string property)
+		{
+			changer.Init(() => _chargeHolder.IsCharged);
+			changer.Init(property);
+		}
 	}
 }
